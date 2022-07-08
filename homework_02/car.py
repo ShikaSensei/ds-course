4923d45fa094544adbf0870931d925b6843a2a43
Leslie Maltsev <shikasensei@gmail.com>
2022-07-08 16:11:36 +0300

    add class Car, based on Vehicle

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,12 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+
+class Car(Vehicle):
+    engine: Engine
+
+    def set_engine(self, engine: Engine):
+        self.engine = engine
