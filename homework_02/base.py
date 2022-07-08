b1ba86973393e5f7e9fe56694d9093ad2c313d86
Leslie Maltsev <shikasensei@gmail.com>
2022-07-08 16:10:24 +0300

    add base class Vehicle

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,21 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    def __init__(self, weight=100, fuel=0, fuel_consumption=5):
+        self.started = False
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if self.fuel <= 0:
+            raise LowFuelError("Fuel should be > 0")
+        self.started = True
+
+    def move(self, distance):
+        required_fuel = distance * self.fuel_consumption
+        if required_fuel > self.fuel:
+            raise NotEnoughFuel("Not enough fuel to proceed")
+        self.fuel -= required_fuel
